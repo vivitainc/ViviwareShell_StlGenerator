ee456fcc992850ff6bcfd022e92a601041073532
kassy-vvt <kashimoto@piccolo.co>
2020-06-14 16:29:05 +0900

    ビルド通す

---
+++ app/src/main/java/za/co/house4hack/paint3d/MainActivity.kt
@@ -0,0 +1,11 @@
+package za.co.house4hack.paint3d
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
